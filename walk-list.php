f560cf0100002345ba5f0cfc033fbe1625305c25
pwolanin <pwolanin@49851.no-reply.drupal.org>
2012-09-01 19:05:23 -0400

    first pass fix for walk list generator

--- walk-list.php
+++ walk-list.php
@@ -10,42 +10,34 @@
  */
 
 require_once dirname(__FILE__) . '/db-helper.php';
-// Optionally include the DB url from a file.
-if (file_exists('./settings.php')) {
-  include './settings.php';
-}
 
-if (count($argv) < 2 && isset($db_url)) {
+if (count($argv) < 2) {
   exit("usage: {$argv[0]} viewname");
-}
-elseif (count($argv) < 3 && !isset($db_url)) {
-  exit("usage: {$argv[0]} viewname mysqli_connection_url\n\nA valid connection URL looks like 'mysqli://myname:pass@127.0.0.1:3306/voterdbname'\n");
 }
 
 $viewname = db_escape_table($argv[1]);
-if (isset($argv[2])) {
-  $db_url = $argv[2];
-}
-
-$active_db = db_connect($db_url);
 
 $html_columns = array(
   ' ' => ' ',
-  'code' => 'Y LY U LN N W R',
+  'code_obama' => 'Y LY U LN N W R',
+  'code_local' => 'Y LY U LN N W R',
+  'targeted' => 'targeted',
   'first_name' => 'first_name',
   'last_name' => 'last_name',
   'num' => 'street_number',
   'street_name' => 'street_name',
   'unit' => array('suffix_a', 'suffix_b', 'apt_unit_no'),
-  'birth_date' => 'birth_date',
+  'DOB' => 'date_of_birth',
   'party' => 'party_code',
   'note' => ' ',
 );
 
 $csv_columns = array(
   'voter_id' => 'voter_id',
-  'code' => ' ',
+  'code_obama' => ' ',
+  'code_local' => ' ',
   'note' => ' ',
+  'targeted' => 'targeted',
   'first_name' => 'first_name',
   'last_name' => 'last_name',
   'number' => 'street_number',
@@ -53,12 +45,12 @@ $csv_columns = array(
   'unit' => array('suffix_a', 'suffix_b', 'apt_unit_no'),
 );
 
-$result = db_query($active_db, "
-SELECT v.*, vi.home_phone FROM voters v 
-LEFT JOIN van_info vi ON v.voter_id = vi.voter_id
+$result = db_query("
+SELECT v.*, IF (target.voter_id, 'Y', '') AS targeted FROM voters v 
 INNER JOIN voter_doors vd ON v.voter_id = vd.voter_id
-WHERE vd.door IN (SELECT vd.door FROM $viewname v INNER JOIN voter_doors vd ON v.voter_id = vd.voter_id)
-ORDER BY v.street_name ASC, v.street_number ASC, v.suffix_a, v.suffix_b, v.apt_unit_no ASC, v.last_name ASC");
+LEFT JOIN $viewname target ON v.voter_id = target.voter_id
+WHERE v.party_code != 'REP' AND vd.door IN (SELECT vd.door FROM $viewname v INNER JOIN voter_doors vd ON v.voter_id = vd.voter_id)
+ORDER BY v.street_name ASC, v.street_num_int ASC, v.suffix_a, v.suffix_b, v.apt_unit_no ASC, v.last_name ASC")->fetchAll(PDO::FETCH_ASSOC);
 
 
 $time = date('Y-m-d_h-i');
@@ -72,10 +64,10 @@ $head = <<<EOHEAD
 <title>{$viewname}_{$time}</title>
 <style>
 table#walk-list {
-  width: 68em;
+  width: 85em;
 }
 #walk-list tr th.note {
-  padding-right: 10em;
+  padding-right: 5em;
 }
 #walk-list tr td {
   background-color: #fff;
@@ -104,7 +96,7 @@ fputcsv($csv_fp, array_keys($csv_columns));
 
 $doors = array();
 $zebra = 0;
-while ($row = db_fetch_array($result)) {
+foreach ($result as $row) {
   $html_row = build_row_cells($row, $html_columns);
   $address = $row['street_number'] . '|' . $row['street_name'] . '|' . $row['apt_unit_no'] . '|' . $row['suffix_a'] . '|' . $row['suffix_b'];
   $doors[$address] = TRUE;
