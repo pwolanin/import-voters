2badf6375bdaec567ed8f56bc8dab6480a14dc25
pwolanin <pwolanin@49851.no-reply.drupal.org>
2012-11-08 12:24:35 -0500

    extra helper for converting tab delimited to pipe delimited (e.g. mysql -B output to pipe)

---
+++ tab-to-pipe.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+/**
+ * A simple CLI script to transform tab-delimited input
+ * on stdin to csv output on stdout
+ */
+
+// Don't wait for input if there is none already.
+stream_set_blocking(STDIN, 0);
+
+while (($line = fgets(STDIN)) !== FALSE) {
+  $line = rtrim($line, "\r\n");
+  $parts = explode("\t", $line);
+  fputcsv(STDOUT, $parts, '|');
+}
